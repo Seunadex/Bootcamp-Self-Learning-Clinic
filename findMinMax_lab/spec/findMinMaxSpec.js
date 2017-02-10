112d639fe2ea07d1ccaf7e66712bd91fa839ab38
Adekunle Seun <oladeleadekunle94@gmail.com>
2017-02-10 13:24:35 +0100

    Update spec code

--- findMinMax_lab/spec/findMinMaxSpec.js
+++ findMinMax_lab/spec/findMinMaxSpec.js
@@ -9,7 +9,7 @@ var findMinMax = require("../app/findMinMax.js");
 
     describe('Return the min and max number in the list in a new list follows `[min, max]`', function () {
 
-      it('should return [1,4] for [1, 2, 3 , 4]', function () {
+      it('should return [1, 4] for [1, 2, 3 , 4]', function () {
         expect(findMinMax.findMinMax([1, 2, 3, 4])).toEqual([1, 4]);
       });
 
@@ -21,7 +21,7 @@ var findMinMax = require("../app/findMinMax.js");
         expect(findMinMax.findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).toEqual([2, 78]);
       });
 
-      it('should return [1,4] for [1, 20, 3 , 4]', function () {
+      it('should return [1, 20] for [1, 20, 3 , 4]', function () {
         expect(findMinMax.findMinMax([1, 20, 3, 4])).toEqual([1, 20]);
       });
 
@@ -37,15 +37,15 @@ var findMinMax = require("../app/findMinMax.js");
         expect(findMinMax.findMinMax([4, 4, 4, 4])).toEqual([4]);
       });
 
-      it('should return [4] for [9, 9, 9, 9, 9, 9, 9]', function () {
+      it('should return [9] for [9, 9, 9, 9, 9, 9, 9]', function () {
         expect(findMinMax.findMinMax([9, 9, 9, 9, 9, 9, 9])).toEqual([9]);
       });
 
-      it('should return [4] for [0, 0, 0, 0]', function () {
+      it('should return [0] for [0, 0, 0, 0]', function () {
         expect(findMinMax.findMinMax([0, 0, 0, 0])).toEqual([0]);
       });
 
-      it('should return [4] for [29, 29, 29, 29, 29, 29, 29]', function () {
+      it('should return [29] for [29, 29, 29, 29, 29, 29, 29]', function () {
         expect(findMinMax.findMinMax([29, 29, 29, 29, 29, 29, 29])).toEqual([29]);
       });
 
